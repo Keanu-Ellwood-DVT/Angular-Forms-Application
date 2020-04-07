e89a2c21a5877dfeedd7180d8b70eec01419cbe6
Keanu Ellwood <KEllwood@jhb.dvt.co.za>
2020-04-07 08:41:32 +0200

    Add phone number number validation

--- src/app/forms/validators/phone-number-validation-factory.ts
+++ src/app/forms/validators/phone-number-validation-factory.ts
@@ -1,2 +1,13 @@
+import { ValidatorFn, AbstractControl } from '@angular/forms';
+
 export class PhoneNumberValidationFactory {
+    static SouthAfricanPhoneNumber(): ValidatorFn {
+        return (control: AbstractControl) => {
+            if (!control.value) {
+                return null;
+            }
+            const isValid = /^(\+27|27|0)[0-9]{2}( |-)?[0-9]{3}( |-)?[0-9]{4}( |-)?(x[0-9]+)?(ext[0-9]+)?/.test(control.value);
+            return isValid ? null : { localPhoneNumber: true };
+        };
+    }
 }
